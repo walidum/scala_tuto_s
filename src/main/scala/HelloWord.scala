60d004b92d46f67ade8052c6e6b97c5c40768a7f
Walid Boubechtoula <dw_boubechtoula@esi.dz>
2021-07-04 11:03:20 +0100

    scala while loop

--- src/main/scala/HelloWord.scala
+++ src/main/scala/HelloWord.scala
@@ -1,24 +1,10 @@
 object HelloWord {
   def main(args: Array[String]): Unit ={
-    val  name = "Scala"
-    val age = 3.2
-    println(name + " for (scalable languge) " +  age )
-    println(s"$name for (scalable languge) $age")
-    println(f"$name%s for (scalable languge) $age%f")
-     if (age == 0) {
-       println(2131321)
-     }
-    else {
-       println(212)
-     }
-    var i =0 ;
-    while (i <= 2 ){
-      println(i)
-      i+=1
+    var x = 0
+    while (x<5){
+      println(s"x = $x")
+      x +=1
     }
-
-    val isOk = if(i > 3)  true else false
-    println(isOk)
   }
 
 }
