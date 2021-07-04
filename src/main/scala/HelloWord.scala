d7d358ef56b7db491cff7daf4563a21131a7d7ce
Walid Boubechtoula <dw_boubechtoula@esi.dz>
2021-07-04 11:41:28 +0100

    scala methodes

--- src/main/scala/HelloWord.scala
+++ src/main/scala/HelloWord.scala
@@ -1,11 +1,20 @@
 object HelloWord {
-  def main(args: Array[String]): Unit = {
-    val age = 100
-    val res = age match {
-      case 20 => 20
-      case ___ => -1
-    }
+  def FirstFunction(x: Int, y: Int): Int = {
+    return x + y
+  }
 
-    println(res)
+  object Math {
+    def add(x: Int = 0, y: Int = 0): Int = x + y
+
+    def *(x: Int) = x * x
+
+    def print(s: String): Unit = println(s)
+  }
+
+  def main(args: Array[String]): Unit = {
+    var add = (x: Int, y: Int) => x * y
+    println(Math.add(2))
+    println(Math.*(2))
+    Math.print("ME ")
   }
 }
