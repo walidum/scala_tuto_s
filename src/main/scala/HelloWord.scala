32a833bca1b2827d48aa1de5ba0fa7faa15018d1
Walid Boubechtoula <dw_boubechtoula@esi.dz>
2021-07-04 14:22:26 +0100

    lists scala

--- src/main/scala/HelloWord.scala
+++ src/main/scala/HelloWord.scala
@@ -2,6 +2,12 @@ import java.util.Date
 
 object HelloWord {
   val aoo = () => 12
+  val el = Array(12, 1321, 1, 3, 132, 1, 31, 3).filter(e => e == 0)
+
+  var num = 30
+  val add = (x: Int) => x + num
+
+  def add2(x: Int) = (y: Int) => x + y
 
   object Math {
     def add(x: Int = 0, y: Int = 0): Int = x + y
@@ -19,12 +25,27 @@ object HelloWord {
     }
   }
 
-  var num = 30
-  val add = (x: Int) => x + num
+  object Arrays {
+    val tab: Array[Int] = new Array[Int](5)
 
-  def add2(x: Int) = (y: Int) => x + y
+    def printArray = () => {
+      for (i <- 0 until tab.length) {
+        println(tab(i))
+      }
+    }
+  }
+
+  object Lists {
+    var list: List[Int] = List(456456, 4, 654, 654, 4, 65)
+
+    def printList = () => {
+      list = 12 :: list
+      list = list :+ 50
+      list.foreach(e => println(e))
+    }
+  }
 
   def main(args: Array[String]): Unit = {
-    println(add2(20)(3))
+    Lists.printList()
   }
 }
