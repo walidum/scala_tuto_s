674e0764725333f9d40f3715fa33be9566dd379b
Walid Boubechtoula <dw_boubechtoula@esi.dz>
2021-07-04 11:59:06 +0100

    partially Applied Functions

--- src/main/scala/HelloWord.scala
+++ src/main/scala/HelloWord.scala
@@ -1,3 +1,4 @@
+import java.util.Date
 
 object HelloWord {
   val aoo = () => 12
@@ -12,9 +13,21 @@ object HelloWord {
     def math(x: Double, y: Double, f: (Double, Double) => Double): Double = f(x, y)
   }
 
+  object Log {
+    def log(date: Date, message: String) = {
+      println(s"$date $message")
+    }
+  }
+
   def main(args: Array[String]): Unit = {
     val sum = (a: Int, b: Int, c: Int) => a + b + c
     val f = sum(10, 30, _: Int)
     println(f(100))
+
+    val date = new Date
+    val newLog = Log.log(date, _: String);
+    newLog("Message 01 ")
+    newLog("Message 02")
+    newLog("Message 03 ")
   }
 }
