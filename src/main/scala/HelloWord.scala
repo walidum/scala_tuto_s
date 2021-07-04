8afd0509f691f0bb0e9da5b2c8aa05a59e4a45c6
Walid Boubechtoula <dw_boubechtoula@esi.dz>
2021-07-04 11:50:24 +0100

    scala methodes

--- src/main/scala/HelloWord.scala
+++ src/main/scala/HelloWord.scala
@@ -1,7 +1,5 @@
 object HelloWord {
-  def FirstFunction(x: Int, y: Int): Int = {
-    return x + y
-  }
+  val aoo = () => 12
 
   object Math {
     def add(x: Int = 0, y: Int = 0): Int = x + y
@@ -9,12 +7,16 @@ object HelloWord {
     def *(x: Int) = x * x
 
     def print(s: String): Unit = println(s)
+
+    def mmm(x: Double, y: Double, f: (Double, Double) => Double): Double = f(x, y)
   }
 
   def main(args: Array[String]): Unit = {
+    val res = Math.mmm(20, 30, _ min _)
     var add = (x: Int, y: Int) => x * y
     println(Math.add(2))
     println(Math.*(2))
     Math.print("ME ")
+    val i = aoo
   }
 }
