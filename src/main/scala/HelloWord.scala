e00744887e5782e0f11c0bf09105788a19de7c72
Walid Boubechtoula <dw_boubechtoula@esi.dz>
2021-07-04 11:23:48 +0100

    scala match statement

--- src/main/scala/HelloWord.scala
+++ src/main/scala/HelloWord.scala
@@ -1,19 +1,11 @@
 object HelloWord {
   def main(args: Array[String]): Unit = {
-    for (a <- 1 to 10) {
-      println(s" value of a  = $a")
-    }
-    for (a <- 1 until 10) {
-      println(s" value of a  = $a")
-    }
-
-    val list = List(1, 2, 454, 4787, 78, 7, 787, 87, 87, 8, 7, 8)
-    val res = for {ele <- list; if ele > 10} yield {
-      ele * 2
+    val age = 100
+    val res = age match {
+      case 20 => 20
+      case ___ => -1
     }
 
     println(res)
-
-
   }
 }
