ed4fa9d9bc3bfda8991ad56f9137d2b6d6f81fb8
Walid Boubechtoula <dw_boubechtoula@esi.dz>
2021-07-04 11:06:28 +0100

    scala do while loop

--- src/main/scala/HelloWord.scala
+++ src/main/scala/HelloWord.scala
@@ -1,10 +1,13 @@
 object HelloWord {
-  def main(args: Array[String]): Unit ={
+  def main(args: Array[String]): Unit = {
     var x = 0
-    while (x<5){
+    while (x < 5) {
       println(s"x = $x")
-      x +=1
+      x += 1
     }
+    do {
+      println(s"new x = $x")
+      x += 1
+    } while (x < 10)
   }
-
 }
